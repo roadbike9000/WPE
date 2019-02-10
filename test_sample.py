fe0c706d5487e6945b47a327e5496dee76dfd473
roadbike9000 <roadbike.rjs@gmail.com>
2019-02-09 19:49:03 -0500

    added test raises Python exception

--- test_sample.py
+++ test_sample.py
@@ -1,7 +1,18 @@
 import pytest
 
+
 def func(x):
     return x + 1
 
+
 def test_answer():
     assert func(3) == 5
+
+
+def f():
+    raise SystemExit(1)
+
+
+def test_f_raises_systemexit():
+    with pytest.raises(SystemExit):
+        f()
